14aec3fcc840d32ee782b6591f8b53db66c94212
maxafritz <maxafritz@gmail.com>
2020-04-13 23:45:29 -0500

    Update CreateTeamsAndSPO.ps1

--- Scripts/CreateTeamsAndSPO.ps1
+++ Scripts/CreateTeamsAndSPO.ps1
@@ -4,7 +4,6 @@ DISCLAIMER:
 This sample is provided as is and is not meant for use on a production environment.
 It is provided only for illustrative purposes. The end user must test and modify the
 sample to suit their target environment. 
-
 Microsoft can make no representation concerning the content of this sample. Microsoft
 is providing this information only as a convenience to you. This is to inform you that
 Microsoft has not tested the sample and therefore cannot make any representations 
